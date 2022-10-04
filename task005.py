58de1be5a2cd9d4872999f38fb75eb6a91817915
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-10-04 03:32:46 +0300

    v005

--- task005.py
+++ task005.py
@@ -1,24 +1,48 @@
-from dataclasses import replace
-import re
-import shutil
+# 5 - Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. 
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+# файл первый:
+# AAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCDDDDDDEEEEEFFFFG python is sooooooo coooooool
+# файл второй:
+# сжатый текст.
 
-str_new=[]
-student=open('student.txt', 'r', encoding="utf-8")
-while True:
-        line = student.readline()
-        if not line:
-            break
-        num = re.findall('(\d+)', line)
-        num = int(num[0])
-        if num > 4:
-            str_new.append(line.upper())
-        else:
-            str_new.append(line)
-student.close()
-student_new=open('student_new.txt', 'w',encoding="utf-8")
-for element in str_new:
-     student_new.write(element)
-student_new.close()
-shutil.copyfile('student_new.txt', 'student.txt')
+def rle_encode(s):
+    encoding = ""
+    i = 0
+    while i < len(s):
+        count = 1
+        while i + 1 < len(s) and s[i] == s[i + 1]:
+            count = count + 1
+            i = i + 1
+        encoding += str(count) + s[i]
+        i = i + 1
+ 
+    return encoding
 
+def rle_decode(data): 
+    decode = '' 
+    count = ''
+    for char in data: 
+        if char.isdigit():
+           count += char 
+        else: 
+            decode += char * int(count) 
+            count = '' 
+    return decode
+
+with open('file_first.txt', 'w') as data:
+    data.write('AAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCDDDDDDEEEEEFFFFG python is sooooooo coooooool')
+
+with open('file_first.txt', 'r') as file:
+    first_string = file.read()
+
+compressed_text=rle_encode(first_string)
+
+with open('file_second.txt', 'w') as file:
+    decoded_string = rle_decode(compressed_text)
+    file.write(decoded_string)
+
+print('First string: \t\t' + first_string)
+print('Decode string: \t\t' + decoded_string)
+print('Compressed_text:\t' + compressed_text)
+print(f'Compress ratio: \t {round(len(first_string) / len(compressed_text), 1)}')
 
